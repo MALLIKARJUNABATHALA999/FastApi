0274dbe49c437dfba07910806ab2f11b0eb0f6aa
mallikarjuna <mallikarjunabathala999@gmail.com>
2024-10-26 06:12:29 -0700

    created and user and block no connect with relation

--- blog/main.py
+++ blog/main.py
@@ -16,7 +16,7 @@ def get_db():
     finally:
         db.close()
 
-@myapp.post('/blog',status_code=201)
+@myapp.post('/blog',status_code=201,tags=["blogs"])
 def create(request:schemas.Blog,db: Session=Depends(get_db)):
     new_blog=models.Blog(title=request.title,body=request.body)
     db.add(new_blog)
@@ -24,12 +24,12 @@ def create(request:schemas.Blog,db: Session=Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@myapp.get('/blog',status_code=status.HTTP_201_CREATED,response_model=List[schemas.ShowBlog])
+@myapp.get('/blog',status_code=status.HTTP_201_CREATED,response_model=List[schemas.ShowBlog],tags=["blogs"])
 def all(db: Session=Depends(get_db)):
     blogs=db.query(models.Blog).all()
     return blogs
 
-@myapp.get('/blog/{id}',status_code=200,response_model=schemas.ShowBlog)
+@myapp.get('/blog/{id}',status_code=200,response_model=schemas.ShowBlog,tags=["blogs"])
 def show(id,response:Response,db:Session=Depends(get_db)):
     blog=db.query(models.Blog).filter(models.Blog.id==id).first()
     if not blog:
@@ -38,7 +38,7 @@ def show(id,response:Response,db:Session=Depends(get_db)):
         #return {"detail":f"Blog with the id {id} is not avaailable"}
     return blog
 
-@myapp.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
+@myapp.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT,tags=["blogs"])
 def destroy(id,db:Session=Depends(get_db)):
     blog=db.query(models.Blog).filter(models.Blog.id==id)
     if not blog.first():
@@ -47,7 +47,7 @@ def destroy(id,db:Session=Depends(get_db)):
     db.commit()
     return {"done"}
 
-@myapp.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
+@myapp.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED,tags=["blogs"])
 def update(id,request:schemas.Blog,db:Session=Depends(get_db)):
     blog=db.query(models.Blog).filter(models.Blog.id==id)
     if not blog.first:
@@ -59,7 +59,7 @@ def update(id,request:schemas.Blog,db:Session=Depends(get_db)):
 
 
 pwd_cxt=CryptContext(schemes=["bcrypt"],deprecated="auto")
-@myapp.post('/user',response_models=schemas.ShowUser)
+@myapp.post('/user',response_model=schemas.ShowUser)
 def create_user(request:schemas.User,db: Session=Depends(get_db)):
     
     new_user=models.User(name=request.name,email=request.email,password=hashing.Hash.bcrypt(request.password),phonenumber=request.phonenumber)
@@ -68,8 +68,14 @@ def create_user(request:schemas.User,db: Session=Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
-@myapp.get("/user")
+@myapp.get("/user",tags=["user"])
 def all(db: Session=Depends(get_db)):
     blogs=db.query(models.User).all()
     return blogs
 
+@myapp.get("/user{id}",response_model=schemas.ShowUser,tags=["user"])
+def get_user(id:int,db: Session=Depends(get_db)):
+    user=db.query(models.User).filter(models.User.id==id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"blog with the id {id} is not available")
+    return user
